bf3c84f97a72e5ec7995993462828ac48088b6bf
Animesh Pattnaik <pattnaik.animesh@gmail.com>
2024-08-04 21:09:05 +0530

    navbar and login improved

--- src/app/login/Login.jsx
+++ src/app/login/Login.jsx
@@ -42,24 +42,21 @@ const Login = () => {
   };
 
   return (
-    <div className="grid grid-cols-12">
-      <div className="col-span-4 col-start-5 ">
-        <div className="py-5"></div>
-
-        <h1 className="text-3xl text-center">Login Here </h1>
-
+    <div className="flex items-center justify-center">
+      <div className="w-full max-w-md p-8 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-semibold text-center mb-6">Login Here</h1>
         <form action="#!" onSubmit={loginFormSubmitted}>
-          <div className="mt-3">
+          <div className="mb-4">
             <label
               htmlFor="user_email"
-              className="block text-sm font-medium mb-2 ps-2"
+              className="block text-sm font-medium mb-2"
             >
               Email
             </label>
             <input
               type="email"
-              className="w-full p-3 rounded-2xl bg-gray-800 focus:ring-gray-400-100 border border-gray-800"
-              placeholder="Enter here"
+              className="w-full p-3 rounded-lg text-black bg-gray-200 border border-gray-300 focus:ring-2 focus:ring-blue-500"
+              placeholder="Enter your email"
               id="user_email"
               name="user_email"
               onChange={(event) => {
@@ -71,18 +68,18 @@ const Login = () => {
               value={loginData.email}
             />
           </div>
-          {/* password */}
-          <div className="mt-3">
+          {/* Password */}
+          <div className="mb-6">
             <label
               htmlFor="user_password"
-              className="block text-sm font-medium mb-2 ps-2"
+              className="block text-sm font-medium mb-2"
             >
               Password
             </label>
             <input
               type="password"
-              className="w-full p-3 rounded-2xl bg-gray-800 focus:ring-gray-400-100 border border-gray-800"
-              placeholder="Enter here"
+              className="w-full p-3 rounded-lg text-black bg-gray-200 border border-black-300 focus:ring-2 focus:ring-blue-500"
+              placeholder="Enter your password"
               id="user_password"
               onChange={(event) => {
                 setLoginData({
@@ -94,23 +91,22 @@ const Login = () => {
             />
           </div>
 
-          <div className="mt-3 text-center">
+          <div className="flex flex-col gap-3 items-center">
             <button
               type="submit"
-              className="px-3 py-2 bg-green-600  rounded hover:bg-green-400"
+              className="w-full px-4 py-2 bg-gradient-to-r from-blue-400 to-purple-600 text-white rounded-lg focus:border-black focus:outline-none focus:ring-2 focus:ring-black"
             >
               Login
             </button>
             <button
               type="button"
-              className="px-3 py-2 bg-orange-600 ms-3 rounded hover:bg-orange-400"
+              className="w-full px-4 py-2 bg-gradient-to-r from-blue-400 to-purple-600 text-white rounded-lg focus:border-black focus:outline-none focus:ring-2 focus:ring-black"
             >
               Reset
             </button>
           </div>
         </form>
       </div>
-      {/* {JSON.stringify(loginData)} */}
     </div>
   );
 };
--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -25,7 +25,7 @@ const CustomNavbar = () => {
   }
 
   return (
-    <nav className="bg-gradient-to-r from-blue-400 to-purple-600 h-16 py-2 px-6 md:px-36 flex justify-between items-center">
+    <nav className="bg-gradient-to-r from-blue-400 to-purple-600 h-16 py-2 px-6 md:px-36 flex justify-between items-center relative">
       <div className="brand">
         <h1 className="text-2xl font-semibold text-white">
           <a href="#!">Work Manager</a>
@@ -85,39 +85,39 @@ const CustomNavbar = () => {
           )}
         </ul>
       </div>
-      <div className="md:hidden">
+      <div className="md:hidden relative">
         <button className="text-white hover:text-blue-200" onClick={() => setIsMenuOpen(!isMenuOpen)}>
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
           </svg>
         </button>
         {isMenuOpen && (
-          <div className="absolute top-16 right-0 bg-blue-600 w-full flex flex-col items-center">
+          <div className="absolute top-16 right-0 bg-gradient-to-r from-blue-400 to-purple-600 w-64 p-4 rounded-lg shadow-lg transition-transform transform translate-y-2 opacity-90">
             <ul className="flex flex-col space-y-3">
               {context.user && (
                 <>
                   <li>
-                    <Link href="/" className="text-white text-center justify-center hover:text-blue-200" onClick={() => setIsMenuOpen(false)}>
+                    <Link href="/" className="text-white text-center hover:text-blue-200" onClick={() => setIsMenuOpen(false)}>
                       Home
                     </Link>
                   </li>
                   <li>
-                    <Link href="/add-task" className="text-white text-center justify-center hover:text-blue-200" onClick={() => setIsMenuOpen(false)}>
+                    <Link href="/add-task" className="text-white text-center hover:text-blue-200" onClick={() => setIsMenuOpen(false)}>
                       Add Task
                     </Link>
                   </li>
                   <li>
-                    <Link href="/show-tasks" className="text-white text-center justify-center hover:text-blue-200" onClick={() => setIsMenuOpen(false)}>
+                    <Link href="/show-tasks" className="text-white text-center hover:text-blue-200" onClick={() => setIsMenuOpen(false)}>
                       Show Tasks
                     </Link>
                   </li>
                   <li>
-                    <Link href="/profile/user" className="text-white text-center justify-center hover:text-blue-200" onClick={() => setIsMenuOpen(false)}>
+                    <Link href="/profile/user" className="text-white text-center hover:text-blue-200" onClick={() => setIsMenuOpen(false)}>
                       {context.user.name}
                     </Link>
                   </li>
                   <li>
-                    <button onClick={() => { doLogout(); setIsMenuOpen(false); }} className="text-white justify-center text-center hover:text-blue-200">
+                    <button onClick={() => { doLogout(); setIsMenuOpen(false); }} className="text-white text-center hover:text-blue-200">
                       Logout
                     </button>
                   </li>
@@ -126,12 +126,12 @@ const CustomNavbar = () => {
               {!context.user && (
                 <>
                   <li>
-                    <Link href="/login" className="text-white hover:text-blue-200" onClick={() => setIsMenuOpen(false)}>
+                    <Link href="/login" className="text-white text-center hover:text-blue-200" onClick={() => setIsMenuOpen(false)}>
                       Login
                     </Link>
                   </li>
                   <li>
-                    <Link href="/signup" className="text-white hover:text-blue-200" onClick={() => setIsMenuOpen(false)}>
+                    <Link href="/signup" className="text-white text-center hover:text-blue-200" onClick={() => setIsMenuOpen(false)}>
                       Signup
                     </Link>
                   </li>
